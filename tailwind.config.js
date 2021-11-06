e0a160301d6841faaee987a2a5e52a45a6083492
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2021-11-06 13:23:20 +0700

    Tailwind config instruction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      /**
+       * This will be used if you have a static content stored in cms
+       * so tailwind wouldn't purge the css class from the transpiled file(s)
+      */
+      safelist: []
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      /**
+       * If you want to add your custom classes to tailwind, do it here.
+      */
+    //  colors: {
+    //    primary: 'red' // this will transpiled as text-primary, bg-primary, etc
+    //  }
+    },
   },
   variants: {
     extend: {},
